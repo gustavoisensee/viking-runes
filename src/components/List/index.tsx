aa6b0b127136f1e79e00c522ac178d3c63564e90
Gustavo Isensee <gustavo.isensee@gmail.com>
2025-01-05 01:55:52 +0000

    feat: add translator field

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import elderRunes from '../../data/elder-runes.json';
 import Item from '../Item';
 
@@ -6,6 +6,7 @@ import Item from '../Item';
 const List = () => {
   const [filter, setFilter] = useState(0);
   const [search, setSearch] = useState('');
+  const [translate, setTranslate] = useState('gustavo');
 
   const options = elderRunes.runes.filter(option => {
     if (filter === 1) {
@@ -23,26 +24,58 @@ const List = () => {
     setFilter(Number(e.target.value));
   }
 
+  const translated = useMemo(() => {
+    return translate.split('').map(char => {
+      if (char === ' ') return ' ';
+      const rune = elderRunes.runes.find(r => r.english.toLowerCase().includes(char.toLowerCase()));
+      return rune ? rune.rune : ' ';
+    }).join('');
+  }, [translate]);
+
   return (
     <>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-2'>
-        <select
-          className="select select-bordered w-full max-w-xs"
-          onChange={handleFilter}
-        >
-          <option value={0} selected={filter === 0}>Choose your filter</option>
-          <option value={1} selected={filter === 1}>English alphabet</option>
-          <option value={2} selected={filter === 2}>Nordic alphabet</option>
-          <option value={3} selected={filter === 3}>Meaning</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-          value={search}
-          disabled={filter === 0}
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        <div className="form-control">
+          <label className="label">Filter</label>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={handleFilter}
+          >
+            <option value={0} selected={filter === 0}>Choose your filter</option>
+            <option value={1} selected={filter === 1}>English alphabet</option>
+            <option value={2} selected={filter === 2}>Nordic alphabet</option>
+            <option value={3} selected={filter === 3}>Meaning</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="label">&nbsp;</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={search}
+            disabled={filter === 0}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-2'>
+        <div className="form-control">
+          <label className="label">Translate your word</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={translate}
+            onChange={(e) => setTranslate(e.target.value)}
+          />
+        </div>
+
+        <div className="form-control flex justify-center">
+          <label className="label">&nbsp;</label>
+          <span className="text-2xl align-middle">{translated}</span>
+        </div>
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
